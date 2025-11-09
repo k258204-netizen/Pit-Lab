342b0db157174c1d9238269a22a492fb79f2a4c5
k258204-netizen <k258204@nu.edu.pk>
2025-11-09 18:39:21 +0500

    Create Calculator.java

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Calculator {
+    public int add(int a, int b) { return a + b; }
+    public int divide(int a, int b) { return a / b; }
+    public boolean isPositive(int x) { return x > 0; }
+}
